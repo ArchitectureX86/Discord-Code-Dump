9225a7d725d0d79ad8fa367f96f957558664aa94
cum <danzfiledrive@gmail.com>
2026-09-11 02:42:37 +1200

    Iunno.

---
+++ Discord.py Example/app - stats.py
@@ -0,0 +1,29 @@
+import discord
+import psutil
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+    
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+        
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+        
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+        
+    if message.content.startswith('$cpu'):
+        await message.channel.send(f'{psutil.cpu_percent(interval=None)}!')
+
+client.run('MTU0NzQzMjU5MTM4NzM5ODIwNg.G_ayvL.Vu3WNdeF0yHMVWioH9vL6gVDhtPLRi1iapnx7Y')
